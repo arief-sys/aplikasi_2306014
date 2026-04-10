4b29a8d05e604c981520141662f7552161e42609
Arief Prasetya Nugraha <2306014@itg.ac.id>
2026-04-10 12:45:22 +0700

    Routing dan Navigasi

---
+++ lib/home_page.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  final String name;
+  final String imageUrl;
+  final int price;
+  final String description;
+
+  HomePage({
+    required this.name,
+    required this.imageUrl,
+    required this.price,
+    required this.description,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Halaman Home"),
+        backgroundColor: Colors.indigo,
+      ),
+      body: Column(
+        children: [
+          Image.network(imageUrl, width: 200, height: 300, fit: BoxFit.cover),
+          Text(name, style: TextStyle(fontWeight: .bold, fontSize: 16)),
+          Text(
+            price.toString(),
+            style: TextStyle(
+              color: Colors.orange,
+              fontWeight: .bold,
+              fontSize: 20,
+            ),
+          ),
+          ElevatedButton(
+            child: Text("Lihat Detail", style: TextStyle(fontSize: 14)),
+            onPressed: () {
+              Navigator.pushNamed(context, '/second');
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,128 +1,35 @@
 import 'package:flutter/material.dart';
+import 'second_page.dart';
+import 'home_page.dart';
 
 void main() {
-  runApp(const MyApp(name: 'Teknik Informatika', year: 2026));
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  final String name;
-  final int year;
-
-  const MyApp({super.key, required this.name, required this.year});
-
+  // membuat data produk
+  final String name = 'Produk A';
+  final int price = 2000000;
+  final String imageUrl = 'https://picsum.photos/id/0/5000/3333';
+  final String description = 'Deskripsi produk pertama';
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.blue,
-          title: Text('$name $year'),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => HomePage(
+          name: name,
+          imageUrl: imageUrl,
+          price: price,
+          description: description,
         ),
-        body: const NumberScreen(),
-      ),
-    );
-  }
-}
-
-class NumberScreen extends StatefulWidget {
-  const NumberScreen({super.key});
-
-  @override
-  State<NumberScreen> createState() => _NumberScreenState();
-}
-
-class _NumberScreenState extends State<NumberScreen> {
-  String name = 'Gelas Putih';
-  String photo = 'https://picsum.photos/id/30/367/267';
-  int qty = 0;
-  int harga = 15000;
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        margin: const EdgeInsets.all(10),
-        child: Column(
-          children: [
-            Card(
-              child: Padding(
-                padding: const EdgeInsets.all(10),
-                child: Row(
-                  children: [
-                    // Gambar
-                    Image.network(
-                      photo,
-                      width: 100,
-                      height: 100,
-                      fit: BoxFit.cover,
-                    ),
-
-                    const SizedBox(width: 10),
-
-                    // Info produk
-                    Expanded(
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            name,
-                            style: const TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: 16,
-                            ),
-                          ),
-                          const SizedBox(height: 5),
-                          Text(
-                            'Rp $harga',
-                            style: const TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: 16,
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-
-                    // Tombol qty
-                    Row(
-                      children: [
-                        IconButton(
-                          icon: const Icon(Icons.remove),
-                          onPressed: () {
-                            setState(() {
-                              if (qty > 0) {
-                                qty--;
-                              }
-                            });
-                          },
-                        ),
-                        Text(qty.toString()),
-                        IconButton(
-                          icon: const Icon(Icons.add),
-                          onPressed: () {
-                            setState(() {
-                              qty++;
-                            });
-                          },
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
-              ),
-            ),
-
-            const SizedBox(height: 20),
-
-            // Total harga
-            Text(
-              'Total: Rp ${qty * harga}',
-              style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-            ),
-          ],
+        '/second': (context) => SecondPage(
+          name: name,
+          imageUrl: imageUrl,
+          price: price,
+          description: description,
         ),
-      ),
+      },
     );
   }
 }
